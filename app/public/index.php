a958a16248988c7a61cbe0bb074a97bd909a7174
RodrigoBange <rodrigo99@live.nl>
2022-11-18 14:10:24 +0100

    Adjusted values

--- app/public/index.php
+++ app/public/index.php
@@ -2,7 +2,7 @@
     <form action="index.php" method="post">
         <label for="firstname">Name: </label><br>
         <input type="text" id="firstname" name="firstname" value="John"><br>
-        <label for="birthdate">Last name: </label><br>
+        <label for="birthdate">Birthdate: </label><br>
         <input type="date" id="birthdate" name="birthdate" value="2000-01-01"><br>
         <input type="submit" value="Submit" name="SubmitButton">
     </form>
@@ -25,10 +25,9 @@
                 return null;
             }
         }
-        echo '<p> Assignment Get Parameters From Form </p>';
-        echo '<p>' . $name . " " . $birthdate . "\r\n" . '</p>';
+        echo "<p> Assignment Get Parameters From Form </p>";
+        echo "<p>Name: $name $birthdate</p>";
     }
-
 ?>
 
 <?php
@@ -62,13 +61,22 @@
     echo '<p> Assignment Get Parameters From URL </p>';
     // If both values are set
     if (!empty($name) && !empty($birthdate)) {
-        echo '<p>' . $name . " " . $birthdate . '</p>';
+        echo "<p>Name: $name Birthdate: $birthdate</p>";
     }
     else {
-        echo '<p> Not all parameters are set. </p>';
+        echo "<p> Not all parameters are set. </p>";
     }
 ?>
 
 <?php
 echo "Requested URL: " . $_SERVER['REQUEST_URI'];
+?>
+
+
+
+<?php
+    // The code below displays all errors (Forcing them to be specific)
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 ?>
